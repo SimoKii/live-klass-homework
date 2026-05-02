cf5567c304193a31a2acfb617213c3370aedfe4b
Heeeun Cho <heeeun.spring@gmail.com>
2026-05-02 22:29:25 +0900

    chore: 개발 환경 세팅

---
+++ src/main/java/com/homework/liveklasshomework/LiveKlassHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.homework.liveklasshomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveKlassHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveKlassHomeworkApplication.class, args);
+    }
+
+}
