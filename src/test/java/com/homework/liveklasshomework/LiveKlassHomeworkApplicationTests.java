c46738d6560165b6ccbe9b66b6f4e4e0455d5368
Heeeun Cho <heeeun.spring@gmail.com>
2026-05-06 16:26:16 +0900

    test: 불필요한 MockitoBean 제거

--- src/test/java/com/homework/liveklasshomework/LiveKlassHomeworkApplicationTests.java
+++ src/test/java/com/homework/liveklasshomework/LiveKlassHomeworkApplicationTests.java
@@ -1,15 +1,10 @@
 package com.homework.liveklasshomework;
 
-import com.homework.liveklasshomework.application.enrollment.EnrollmentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class LiveKlassHomeworkApplicationTests {
-
-    @MockitoBean
-    private EnrollmentRepository enrollmentRepository;
 
     @Test
     void contextLoads() {
